3f68a4d9d5c43bf61f4d6caabe56b7aa8500497a
Max Henkel <max@maxhenkel.de>
2020-07-18 14:53:30 +0200

    Add documentation

--- src/main/java/de/maxhenkel/corelib/ClientRegistry.java
+++ src/main/java/de/maxhenkel/corelib/ClientRegistry.java
@@ -7,6 +7,14 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class ClientRegistry {
 
+    /**
+     * Registers a new keybinding
+     *
+     * @param name the translation key
+     * @param category the category
+     * @param keyCode the keycode
+     * @return the keybinding
+     */
     public static KeyBinding registerKeyBinding(String name, String category, int keyCode) {
         KeyBinding keyBinding = new KeyBinding(name, keyCode, category);
         net.minecraftforge.fml.client.registry.ClientRegistry.registerKeyBinding(keyBinding);
--- src/main/java/de/maxhenkel/corelib/CommonRegistry.java
+++ src/main/java/de/maxhenkel/corelib/CommonRegistry.java
@@ -10,16 +10,30 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
 
+import java.util.function.Consumer;
+
 public class CommonRegistry {
 
+    /**
+     * Creates a new network channel
+     * @param modId the mod ID
+     * @param name the name of the channel
+     * @return the channel
+     */
     public static SimpleChannel registerChannel(String modId, String name) {
         return NetworkRegistry.newSimpleChannel(new ResourceLocation(modId, name), () -> "1.0.0", s -> true, s -> true);
     }
 
-    public static <T extends Message> void registerMessage(SimpleChannel channel, int id, Class<? extends Message> message) {
-        channel.registerMessage(id, Message.class, (msg, buf) -> msg.toBytes(buf), (buf) -> {
+    /**
+     * Registers a new message on the provided network channel
+     * @param channel the channel to register the message on
+     * @param id the packed id (has to be unique)
+     * @param message the message
+     */
+    public static <T extends Message<?>> void registerMessage(SimpleChannel channel, int id, Class<T> message) {
+        channel.registerMessage(id, (Class) message, Message::toBytes, (buf) -> {
             try {
-                Message msg = message.newInstance();
+                Message<?> msg = message.newInstance();
                 return msg.fromBytes(buf);
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -35,29 +49,32 @@ public class CommonRegistry {
 
     /**
      * Registers the provided entity
-     * Note that the entity class has to have a constructor with the parameters {@link EntityType} and {@link World}
+     * Note that the entity class has to have a constructor with the parameters {@link EntityType} and {@link World} or a constructor with the parameter {@link World}
      *
-     * @param modId               the mod ID
-     * @param name                the entity name
-     * @param classification      the entity classification
-     * @param entityClass         the entity class
-     * @param range               the range in blocks where the entity is being updated
-     * @param updateFrequency     the frequency in tich in which te entity gets updated
-     * @param sendVelocityUpdates if the entity should send velocity updates
+     * @param modId           the mod ID
+     * @param name            the entity name
+     * @param classification  the entity classification
+     * @param entityClass     the entity class
+     * @param builderConsumer a consumer that provides the builder to apply properties
      * @return the entity type of the registered entity
      */
-    public static <T extends Entity> EntityType<T> registerEntity(String modId, String name, EntityClassification classification, Class<? extends Entity> entityClass, int range, int updateFrequency, boolean sendVelocityUpdates) {
-        return EntityType.Builder
-                .<T>create((type, world) -> {
+    public static <T extends Entity> EntityType<T> registerEntity(String modId, String name, EntityClassification classification, Class<? extends Entity> entityClass, Consumer<EntityType.Builder<T>> builderConsumer) {
+        EntityType.Builder<T> builder = EntityType.Builder
+                .create((type, world) -> {
                     try {
-                        return (T) entityClass.getConstructor(EntityType.class, World.class).newInstance(type, world);
+                        try {
+                            return (T) entityClass.getConstructor(EntityType.class, World.class).newInstance(type, world);
+                        } catch (NoSuchMethodException e) {
+                            return (T) entityClass.getConstructor(World.class).newInstance(world);
+                        }
                     } catch (Throwable e) {
                         throw new RuntimeException(e);
                     }
-                }, classification)
-                .setTrackingRange(range)
-                .setUpdateInterval(updateFrequency)
-                .setShouldReceiveVelocityUpdates(sendVelocityUpdates).build(modId + ":" + name);
+                }, classification);
+        builderConsumer.accept(builder);
+        EntityType<T> type = builder.build(modId + ":" + name);
+        type.setRegistryName(new ResourceLocation(modId, name));
+        return type;
     }
 
 }
