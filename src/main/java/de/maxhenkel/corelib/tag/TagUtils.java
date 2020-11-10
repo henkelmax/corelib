3f1141e65f554e10228d3f31270a5a7a01ad5ca4
Max Henkel <max@maxhenkel.de>
2020-11-10 11:16:08 +0100

    Add fluid support in tag utils

--- src/main/java/de/maxhenkel/corelib/tag/TagUtils.java
+++ src/main/java/de/maxhenkel/corelib/tag/TagUtils.java
@@ -1,8 +1,10 @@
 package de.maxhenkel.corelib.tag;
 
 import net.minecraft.block.Block;
+import net.minecraft.fluid.Fluid;
 import net.minecraft.item.Item;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.tags.ITag;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
@@ -44,6 +46,25 @@ public class TagUtils {
                 return null;
             } else {
                 return new SingleElementTag<>(item);
+            }
+        }
+    }
+
+    /**
+     * Gets the tag of the provided registry name
+     *
+     * @param name the registry name of the fluid or a fluid tag starting with '#'
+     * @return the tag
+     */
+    public static ITag<Fluid> getFluid(String name) {
+        if (name.startsWith("#")) {
+            return FluidTags.getCollection().get(new ResourceLocation(name.substring(1)));
+        } else {
+            Fluid fluid = ForgeRegistries.FLUIDS.getValue(new ResourceLocation(name));
+            if (fluid == null) {
+                return null;
+            } else {
+                return new SingleElementTag<>(fluid);
             }
         }
     }
