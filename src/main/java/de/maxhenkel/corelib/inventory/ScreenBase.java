5a7e72eeac348fbbd7df9cef11efa033b71aaefb
Max Henkel <max@maxhenkel.de>
2021-01-31 21:23:28 +0100

    Update screenbase and add nonnulllist collector

--- src/main/java/de/maxhenkel/corelib/inventory/ScreenBase.java
+++ src/main/java/de/maxhenkel/corelib/inventory/ScreenBase.java
@@ -9,13 +9,14 @@ import net.minecraft.util.IReorderingProcessor;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
 public class ScreenBase<T extends Container> extends ContainerScreen<T> {
 
-    protected static final int FONT_COLOR = 4210752;
+    public static final int FONT_COLOR = 4210752;
 
     protected ResourceLocation texture;
     protected List<HoverArea> hoverAreas;
@@ -48,10 +49,8 @@ public class ScreenBase<T extends Container> extends ContainerScreen<T> {
 
     public void drawHoverAreas(MatrixStack matrixStack, int mouseX, int mouseY) {
         for (HoverArea hoverArea : hoverAreas) {
-            if (mouseX >= guiLeft + hoverArea.posX && mouseX < guiLeft + hoverArea.posX + hoverArea.width) {
-                if (mouseY >= guiTop + hoverArea.posY && mouseY < guiTop + hoverArea.posY + hoverArea.height) {
-                    renderTooltip(matrixStack, hoverArea.tooltip.get(), mouseX - guiLeft, mouseY - guiTop);
-                }
+            if (hoverArea.tooltip != null && hoverArea.isHovered(guiLeft, guiTop, mouseX, mouseY)) {
+                renderTooltip(matrixStack, hoverArea.tooltip.get(), mouseX - guiLeft, mouseY - guiTop);
             }
         }
     }
@@ -60,19 +59,24 @@ public class ScreenBase<T extends Container> extends ContainerScreen<T> {
         return size - (int) (((float) amount / (float) max) * (float) size);
     }
 
-    protected void drawCentered(MatrixStack matrixStack, ITextComponent text, int y, int color) {
+    public void drawCentered(MatrixStack matrixStack, ITextComponent text, int y, int color) {
         drawCentered(matrixStack, text, xSize / 2, y, color);
     }
 
-    protected void drawCentered(MatrixStack matrixStack, ITextComponent text, int x, int y, int color) {
-        int width = font.getStringWidth(text.getString());
+    public void drawCentered(MatrixStack matrixStack, ITextComponent text, int x, int y, int color) {
+        int width = font.getStringPropertyWidth(text);
         font.func_243248_b(matrixStack, text, x - width / 2, y, color);
     }
 
     public static class HoverArea {
         private final int posX, posY;
         private final int width, height;
+        @Nullable
         private final Supplier<List<IReorderingProcessor>> tooltip;
+
+        public HoverArea(int posX, int posY, int width, int height) {
+            this(posX, posY, width, height, null);
+        }
 
         public HoverArea(int posX, int posY, int width, int height, Supplier<List<IReorderingProcessor>> tooltip) {
             this.posX = posX;
@@ -80,6 +84,36 @@ public class ScreenBase<T extends Container> extends ContainerScreen<T> {
             this.width = width;
             this.height = height;
             this.tooltip = tooltip;
+        }
+
+        public int getPosX() {
+            return posX;
+        }
+
+        public int getPosY() {
+            return posY;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        @Nullable
+        public Supplier<List<IReorderingProcessor>> getTooltip() {
+            return tooltip;
+        }
+
+        public boolean isHovered(int guiLeft, int guiTop, int mouseX, int mouseY) {
+            if (mouseX >= guiLeft + posX && mouseX < guiLeft + posX + width) {
+                if (mouseY >= guiTop + posY && mouseY < guiTop + posY + height) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
---
+++ src/main/java/de/maxhenkel/corelib/item/NonNullListCollector.java
@@ -0,0 +1,79 @@
+package de.maxhenkel.corelib.item;
+
+import net.minecraft.util.NonNullList;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class NonNullListCollector {
+
+    private static final Set<Collector.Characteristics> CH_ID = Collections.unmodifiableSet(EnumSet.of(Collector.Characteristics.IDENTITY_FINISH));
+
+    public static <T> Collector<T, ?, NonNullList<T>> toNonNullList() {
+        return new CollectorImpl<>((Supplier<NonNullList<T>>) NonNullList::create, NonNullList::add, (left, right) -> {
+            left.addAll(right);
+            return left;
+        }, CH_ID);
+    }
+
+    private static class CollectorImpl<T, A, R> implements Collector<T, A, R> {
+        private final Supplier<A> supplier;
+        private final BiConsumer<A, T> accumulator;
+        private final BinaryOperator<A> combiner;
+        private final Function<A, R> finisher;
+        private final Set<Characteristics> characteristics;
+
+        CollectorImpl(Supplier<A> supplier,
+                      BiConsumer<A, T> accumulator,
+                      BinaryOperator<A> combiner,
+                      Function<A, R> finisher,
+                      Set<Characteristics> characteristics) {
+            this.supplier = supplier;
+            this.accumulator = accumulator;
+            this.combiner = combiner;
+            this.finisher = finisher;
+            this.characteristics = characteristics;
+        }
+
+        CollectorImpl(Supplier<A> supplier,
+                      BiConsumer<A, T> accumulator,
+                      BinaryOperator<A> combiner,
+                      Set<Characteristics> characteristics) {
+            this(supplier, accumulator, combiner, castingIdentity(), characteristics);
+        }
+
+        @Override
+        public BiConsumer<A, T> accumulator() {
+            return accumulator;
+        }
+
+        @Override
+        public Supplier<A> supplier() {
+            return supplier;
+        }
+
+        @Override
+        public BinaryOperator<A> combiner() {
+            return combiner;
+        }
+
+        @Override
+        public Function<A, R> finisher() {
+            return finisher;
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return characteristics;
+        }
+    }
+
+    private static <I, R> Function<I, R> castingIdentity() {
+        return i -> (R) i;
+    }
+
+}
