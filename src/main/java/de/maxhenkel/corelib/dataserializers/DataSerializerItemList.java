ced56d3264c976b7ca1d6ed06481b0c7fead6af1
Max Henkel <max@maxhenkel.de>
2022-06-09 16:23:06 +0200

    Rework item list data serializer

--- src/main/java/de/maxhenkel/corelib/dataserializers/DataSerializerItemList.java
+++ src/main/java/de/maxhenkel/corelib/dataserializers/DataSerializerItemList.java
@@ -2,43 +2,41 @@ package de.maxhenkel.corelib.dataserializers;
 
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraft.world.item.ItemStack;
 
 public class DataSerializerItemList {
 
-    public static final EntityDataSerializer<NonNullList<ItemStack>> ITEM_LIST = new EntityDataSerializer<NonNullList<ItemStack>>() {
+    public static EntityDataSerializer<NonNullList<ItemStack>> create() {
+        return new EntityDataSerializer<>() {
 
-        @Override
-        public void write(FriendlyByteBuf packetBuffer, NonNullList<ItemStack> itemStacks) {
-            packetBuffer.writeInt(itemStacks.size());
+            @Override
+            public void write(FriendlyByteBuf packetBuffer, NonNullList<ItemStack> itemStacks) {
+                packetBuffer.writeInt(itemStacks.size());
 
-            for (ItemStack itemStack : itemStacks) {
-                packetBuffer.writeItem(itemStack);
+                for (ItemStack itemStack : itemStacks) {
+                    packetBuffer.writeItem(itemStack);
+                }
             }
-        }
 
-        public NonNullList<ItemStack> read(FriendlyByteBuf buf) {
-            int length = buf.readInt();
-            NonNullList<ItemStack> list = NonNullList.withSize(length, ItemStack.EMPTY);
-            for (int i = 0; i < list.size(); i++) {
-                list.set(i, buf.readItem());
+            @Override
+            public NonNullList<ItemStack> read(FriendlyByteBuf buf) {
+                int length = buf.readInt();
+                NonNullList<ItemStack> list = NonNullList.withSize(length, ItemStack.EMPTY);
+                for (int i = 0; i < list.size(); i++) {
+                    list.set(i, buf.readItem());
+                }
+                return list;
             }
-            return list;
-        }
 
-        public EntityDataAccessor<NonNullList<ItemStack>> createKey(int id) {
-            return new EntityDataAccessor<>(id, this);
-        }
-
-        @Override
-        public NonNullList<ItemStack> copy(NonNullList<ItemStack> itemStacks) {
-            NonNullList<ItemStack> list = NonNullList.withSize(itemStacks.size(), ItemStack.EMPTY);
-            for (int i = 0; i < itemStacks.size(); i++) {
-                list.set(i, itemStacks.get(i).copy());
+            @Override
+            public NonNullList<ItemStack> copy(NonNullList<ItemStack> itemStacks) {
+                NonNullList<ItemStack> list = NonNullList.withSize(itemStacks.size(), ItemStack.EMPTY);
+                for (int i = 0; i < itemStacks.size(); i++) {
+                    list.set(i, itemStacks.get(i).copy());
+                }
+                return list;
             }
-            return list;
-        }
-    };
+        };
+    }
 }
