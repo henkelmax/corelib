d924a685343a808e734aa716e11209483d5faffa
Max Henkel <max@maxhenkel.de>
2026-03-18 14:13:19 +0100

    Fix OBJ rendering

--- src/main/java/de/maxhenkel/corelib/client/obj/OBJRenderUtils.java
+++ src/main/java/de/maxhenkel/corelib/client/obj/OBJRenderUtils.java
@@ -1,10 +1,12 @@
 package de.maxhenkel.corelib.client.obj;
 
+import com.mojang.blaze3d.pipeline.DepthStencilState;
 import com.mojang.blaze3d.pipeline.RenderPipeline;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.rendertype.RenderSetup;
+import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.resources.Identifier;
 import net.minecraft.util.Util;
 import net.neoforged.neoforge.client.event.RegisterRenderPipelinesEvent;
@@ -19,6 +21,7 @@ class OBJRenderUtils {
             .withSampler("Sampler0")
             .withSampler("Sampler2")
             .withVertexFormat(DefaultVertexFormat.ENTITY, VertexFormat.Mode.TRIANGLES)
+            .withDepthStencilState(DepthStencilState.DEFAULT)
             .buildSnippet();
 
     public static final RenderPipeline ENTITY_CUTOUT_TRIANGLES_PIPELINE = RenderPipeline.builder(ENTITY_SNIPPET_TRIANGLES).withLocation("pipeline/entity_cutout_triangles").withShaderDefine("ALPHA_CUTOUT", 0.1F).withSampler("Sampler1").build();
@@ -27,16 +30,16 @@ class OBJRenderUtils {
         event.registerPipeline(ENTITY_CUTOUT_TRIANGLES_PIPELINE);
     }
 
-    public static final Function<Identifier, net.minecraft.client.renderer.rendertype.RenderType> ENTITY_CUTOUT_TRIANGLES = Util.memoize(
-            p_465686_ -> {
-                RenderSetup rendersetup = RenderSetup.builder(ENTITY_CUTOUT_TRIANGLES_PIPELINE)
-                        .withTexture("Sampler0", p_465686_)
+    public static final Function<Identifier, RenderType> ENTITY_CUTOUT_TRIANGLES = Util.memoize(
+            texture -> {
+                RenderSetup state = RenderSetup.builder(ENTITY_CUTOUT_TRIANGLES_PIPELINE)
+                        .withTexture("Sampler0", texture)
                         .useLightmap()
                         .useOverlay()
                         .affectsCrumbling()
                         .setOutline(RenderSetup.OutlineProperty.AFFECTS_OUTLINE)
                         .createRenderSetup();
-                return net.minecraft.client.renderer.rendertype.RenderType.create("entity_cutout_triangles", rendersetup);
+                return RenderType.create("entity_cutout_triangles", state);
             }
     );
 
